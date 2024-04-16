72da246c6021e0c41c4dd75f0d3eba4c1967dedf
Adrian  G L <adrlau@users.noreply.github.com>
2024-04-16 15:31:46 +0200

    Update config.h

--- keyboards/sculpter/config.h
+++ keyboards/sculpter/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT_ID   0x0000
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Adrian G Lauterer
-#define PRODUCT      sculpter
+#define PRODUCT      sculpt-wired
 
 /* key matrix size */
 #define MATRIX_ROWS 8
@@ -94,7 +94,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEBOUNCE 5
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
-//#define MATRIX_HAS_GHOST
+#define MATRIX_HAS_GHOST
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
